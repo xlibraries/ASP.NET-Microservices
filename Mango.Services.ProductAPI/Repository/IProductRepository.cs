222d6f4b8e78b8a307ba7221404551eb5bdb07d4
Aman Agrawal <Aman.Agrawal2@ibm.com>
2022-09-13 14:08:16 +0530

    Created product repository interface

--- Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.Models.DTOs;
+
+namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
-
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductById(int productId);
+        Task<ProductDTO> CreateUpdateProduct(ProductDTO productDTO);
+        Task<bool> DeleteProduct(int productId);
     }
 }
