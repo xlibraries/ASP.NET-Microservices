a8907b09aaca8e27dd89258bafb363ed433918da
Aman Agrawal <Aman.Agrawal2@ibm.com>
2022-09-13 13:38:42 +0530

    Add project files.

---
+++ Mango.Services.ProductAPI/Migrations/20220913080308_AddProductModelToDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    public partial class AddProductModelToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductPrice = table.Column<int>(type: "int", nullable: false),
+                    ProductDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductCategory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductImgURL = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Range(1,1000)]
+        public int ProductPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductImgURL { get; set; }
+    }
+}
