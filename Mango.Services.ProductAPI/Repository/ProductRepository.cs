d86eafe65b85b539b6ddbcc87b909916eff10037
Aman Agrawal <Aman.Agrawal2@ibm.com>
2022-09-13 16:49:35 +0530

    Implementation for getting/ fetching a product/ list of products

---
+++ Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public Task<ProductDTO> CreateUpdateProduct(ProductDTO productDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductDTO> GetProductById(int productId)
+        {
+            Product product = await _db.Products.Where(x => x.ProductID == productId).FirstOrDefaultAsync();
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            IEnumerable<Product> productList = await _db.Products.ToListAsync();
+            return _mapper.Map<List<ProductDTO>>(productList);
+        }
+    }
+}
