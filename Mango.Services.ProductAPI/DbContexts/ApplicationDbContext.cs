c9dd130535c74ac7bb0db6de48a231891530660a
Aman Agrawal <Aman.Agrawal2@ibm.com>
2022-09-15 15:52:41 +0530

    Seeded/ Populated our database

--- Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Mango.Services.ProductAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace Mango.Services.ProductAPI.DbContexts
 {
@@ -11,5 +12,48 @@ namespace Mango.Services.ProductAPI.DbContexts
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 1,
+                ProductName = "Samosa",
+                ProductPrice = 15,
+                ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ProductImgURL = "",
+                ProductCategory = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 2,
+                ProductName = "Paneer Tikka",
+                ProductPrice = 13.99,
+                ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ProductImgURL = "",
+                ProductCategory = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 3,
+                ProductName = "Sweet Pie",
+                ProductPrice = 10.99,
+                ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ProductImgURL = "",
+                ProductCategory = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 4,
+                ProductName = "Pav Bhaji",
+                ProductPrice = 15,
+                ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ProductImgURL = "",
+                ProductCategory = "Entree"
+            });
+        }
     }
 }
