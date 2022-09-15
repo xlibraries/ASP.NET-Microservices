1ea76058c1ba65f66ad6ea14650d65386577fc67
Aman Agrawal <Aman.Agrawal2@ibm.com>
2022-09-15 18:45:45 +0530

    Added images to azure blob storage and configured them in ApplicationDbContext

--- Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -24,7 +24,7 @@ namespace Mango.Services.ProductAPI.DbContexts
                 ProductName = "Samosa",
                 ProductPrice = 15,
                 ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ProductImgURL = "",
+                ProductImgURL = "https://mangoweb.blob.core.windows.net/mango/11.jpg",
                 ProductCategory = "Appetizer"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -33,7 +33,7 @@ namespace Mango.Services.ProductAPI.DbContexts
                 ProductName = "Paneer Tikka",
                 ProductPrice = 13.99,
                 ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ProductImgURL = "",
+                ProductImgURL = "https://mangoweb.blob.core.windows.net/mango/12.jpg",
                 ProductCategory = "Appetizer"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -42,7 +42,7 @@ namespace Mango.Services.ProductAPI.DbContexts
                 ProductName = "Sweet Pie",
                 ProductPrice = 10.99,
                 ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ProductImgURL = "",
+                ProductImgURL = "https://mangoweb.blob.core.windows.net/mango/13.jpg",
                 ProductCategory = "Dessert"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -51,7 +51,7 @@ namespace Mango.Services.ProductAPI.DbContexts
                 ProductName = "Pav Bhaji",
                 ProductPrice = 15,
                 ProductDescription = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ProductImgURL = "",
+                ProductImgURL = "https://mangoweb.blob.core.windows.net/mango/14.jpg",
                 ProductCategory = "Entree"
             });
         }
---
+++ Mango.Services.ProductAPI/Migrations/20220915131212_SeedProducts.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductID", "ProductCategory", "ProductDescription", "ProductImgURL", "ProductName", "ProductPrice" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://mangoweb.blob.core.windows.net/mango/11.jpg", "Samosa", 15.0 },
+                    { 2, "Appetizer", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://mangoweb.blob.core.windows.net/mango/12.jpg", "Paneer Tikka", 13.99 },
+                    { 3, "Dessert", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://mangoweb.blob.core.windows.net/mango/13.jpg", "Sweet Pie", 10.99 },
+                    { 4, "Entree", "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://mangoweb.blob.core.windows.net/mango/14.jpg", "Pav Bhaji", 15.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 4);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
